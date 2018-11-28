b23a08ea50f8f1e76abdc55a3d32eb4c49ba3d71
yev <yev.berezovsky@gmail.com>
2018-11-28 15:34:04 -0500

    Commit Before data-driving our UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
+@Component ({
+  selector: 'app-sidebar',
+  template: `
+  <div id = "sidebar">
+    Sidebar will go here
+  </div>
+`
+})
+export class SidebarComponent {}
+
+@Component({
+  selector: 'app-article',
+  template:
+  `
+  <div>Article Goes Here</div>
+  `
+})
+export class ArticleComponent {}
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <div id = "container">
+  <app-sidebar></app-sidebar>
+    <div id = "content">
+    <app-article></app-article>
+    <app-article></app-article>
+    <app-article></app-article>
+    <app-article></app-article>
+    <app-article></app-article>
+    <app-article></app-article>
+    </div>
+  </div>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Jack!';
 }
